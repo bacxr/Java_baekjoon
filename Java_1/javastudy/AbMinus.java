83446e19dc8740c3901a3aff561d6cbd9d8a833b
bacxr <bacxr@naver.com>
2024-03-08 23:49:41 +0900

    day_1

---
+++ Java_1/javastudy/AbMinus.java
@@ -0,0 +1,14 @@
+package javastudy;
+
+import java.util.Scanner;
+
+public class AbMinus {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.print(a - b);
+		sc.close();
+	}
+}
